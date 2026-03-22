206421be2bd9627de54fbaf92bc8e352053ac4af
videoencrypt <270153426+videoencrypt@users.noreply.github.com>
2026-03-23 00:10:28 +0600

    updated

--- packages/js/scripts/upload-cdn.js
+++ packages/js/scripts/upload-cdn.js
@@ -15,7 +15,7 @@
  *   R2_ACCOUNT_ID
  *   R2_ACCESS_KEY_ID
  *   R2_SECRET_ACCESS_KEY
- *   R2_BUCKET              default: "videncrypt-sdk"
+ *   R2_BUCKET       
  */
 
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
